35908eab62f3286ada7aee797b7d5945968005b6
Pedro Tobo <ptobo@nashgroup.com>
2018-03-13 14:40:11 -0500

    Initial commit

---
+++ src/JsonRPC/Client.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonRPC
+{
+    public class Client
+    {
+        private Uri url;
+        private CookieContainer cookies = new CookieContainer();
+        private ICredentials credentials = null;
+        private HttpWebRequest request = null;
+        private HttpWebResponse response = null;
+        public int timeout { set; get; }
+        public RequestBuilder jsonRequest { set; get; }
+
+        private void initRequest()
+        {
+            try
+            {
+                request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.Accept = "application/json";
+                request.Timeout = (timeout == 0) ? 100 * 1000 : timeout * 1000;
+                request.CookieContainer = cookies;
+
+                if (credentials == null)
+                {
+                    credentials = CredentialCache.DefaultCredentials;
+                }
+
+                request.Credentials = credentials;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Client(Uri url, ICredentials credentials = null)
+        {
+            this.url = url;
+            this.credentials = credentials;
+        }
+
+        public void Authentication(String user, String password)
+        {
+
+        }
+
+        public String Execute(String method, Object parameters = null)
+        {
+            if (jsonRequest == null)
+            {
+                jsonRequest = new RequestBuilder(method, parameters);
+            }
+
+            String json = jsonRequest.Build();
+            jsonRequest = null;
+
+            initRequest();
+
+            byte[] reqBytes = Encoding.UTF8.GetBytes(json);
+            request.ContentLength = reqBytes.Length;
+
+            try
+            {
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(reqBytes, 0, reqBytes.Length);
+                    requestStream.Close();
+                }
+
+                response = (HttpWebResponse)request.GetResponse();
+                cookies = request.CookieContainer;
+                request = null;
+
+                string result;
+
+                using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
+                {
+                    result = rdr.ReadToEnd();
+                    JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+                    JToken value;
+
+                    if (jsonResponse.TryGetValue("error", out value))
+                    {
+                        throw new Exception("Error " + value["code"] + ": " + value["message"]);
+                    }
+                    else if (jsonResponse.TryGetValue("result", out value))
+                    {
+                        return value.ToString();
+                    }
+                    else
+                    {
+                        throw new Exception("Unexpected error: " + result);
+                    }
+                }
+            }
+            catch (WebException wex)
+            {
+                throw wex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ src/JsonRPC/RequestBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonRPC
+{
+    public class RequestBuilder
+    {
+        private Random rnd = new Random();
+        private JObject json;
+        public int id { set; get; }
+        public String method { set; get; }
+        public Object parameters { set; get; }
+
+        public RequestBuilder()
+        {
+            Random random = new Random();
+        }
+
+        public RequestBuilder(String method)
+        {
+            this.method = method;
+        }
+
+        public RequestBuilder(String method, Object parameters)
+        {
+            this.method = method;
+            this.parameters = parameters;
+        }
+
+        public String Build()
+        {
+            json = new JObject();
+            json["jsonrpc"] = "2.0";
+            json["method"] = method;
+            json["id"] = (id == 0) ? rnd.Next(1000, 99999) : id;
+
+            if (parameters != null)
+            {
+                if (parameters.GetType().Equals(typeof(Dictionary<String, String>)))
+                {
+                    Dictionary<String, String> prmts = (Dictionary<String, String>)parameters;
+
+                    if (prmts.Count > 0)
+                    {
+                        var props = new JObject();
+
+                        foreach (KeyValuePair<string, string> kvp in prmts)
+                        {
+                            props.Add(kvp.Key, kvp.Value);
+                        }
+
+                        json.Add(new JProperty("params", props));
+                    }
+                }
+
+                if (parameters.GetType().Equals(typeof(String[])))
+                {
+                    String[] prmts = (String[])parameters;
+
+                    if (prmts.Length > 0)
+                    {
+                        JArray props = new JArray();
+
+                        foreach (var p in prmts)
+                        {
+                            props.Add(p);
+                        }
+
+                        json.Add(new JProperty("params", props));
+                    }
+                }
+
+            }
+
+            return JsonConvert.SerializeObject(json);
+        }
+    }
+}
